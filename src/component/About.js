f5e5e5adee0808955dc1812c0fdfca17c48b8aba
David Bronne <davidbronne@gmail.com>
2021-06-30 12:01:58 +0200

    Create components:Navbar,Home,About,Contact

---
+++ src/component/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const About = () => {
+    <div className="container">
+        <h4 className="center"></h4>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut iste esse velit a nostrum unde corrupti, ipsa autem quae voluptates quaerat dolore repellendus libero officiis obcaecati quia? Corrupti, non quas.</p>
+    </div>
+}
+
+export default About 
---
+++ src/component/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Contact = () => {
+    <div className="container">
+        <h4 className="center"></h4>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut iste esse velit a nostrum unde corrupti, ipsa autem quae voluptates quaerat dolore repellendus libero officiis obcaecati quia? Corrupti, non quas.</p>
+    </div>
+}
+
+export default Contact 
