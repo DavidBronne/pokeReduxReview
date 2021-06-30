b973d0f964fcc895d839dbe96a126aaeca9b4d71
David Bronne <davidbronne@gmail.com>
2021-06-30 14:59:10 +0200

    axios.get API and display map list

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,13 +1,46 @@
-import React from 'react'
+import React, {Component} from 'react';
+import axios from 'axios';
 
-const Home = () => {
-    return (
-    <div className="container">
-        <h4 className="center">Home</h4>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut iste esse velit a nostrum unde corrupti, ipsa autem quae voluptates quaerat dolore repellendus libero officiis obcaecati quia? Corrupti, non quas.</p>
-    </div>
-        
-    )
+class Home extends Component {
+    
+    state = {
+        posts:[]
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts/')
+            .then((res)=>{
+                this.setState(
+                    {posts : res.data.slice(0,10)}
+                )
+            })
+    }
+
+    render () {
+        const {posts} = this.state;
+        const postList = posts.length ? (
+            posts.map((post) => {
+                        return (
+                            <div className="post card" key={post.id}>
+                                <div className="card-content">
+                                    <div className="card-title">{post.title}</div>
+                                    <p>{post.body}</p>
+                                </div>
+                            </div>
+                        )
+                    })
+        ) : (
+                <div className="center">No post yet</div>
+            )
+
+        return (
+            <div className="container">
+                <h4 className="center">Home</h4>
+                {postList}
+            </div>
+                
+            )
+    }
 }
 
 export default Home 
